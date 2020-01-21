4e2c4ef990323d588f5c248811d94ea7d347850b
Alfiado Ngana <alfiadoprojectos@gmail.com>
2020-01-21 11:23:04 +0200

    consultando a API

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,13 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 
-import { View, Text } from 'react-native';
-
+import api from '~/services/api';
 import styles from './styles';
 
-const Welcome = () => (
-  <View style={styles.container}>
-    <Text>Testando</Text>
-  </View>
-);
+export default class Welcome extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+    };
+  }
 
-export default Welcome;
+  saveUser = async username => {
+    await AsyncStorage.setItem('@githuber:username', username);
+  };
+
+  checkUserExists = async username => {
+    const user = await api.get(`/users/${username}`);
+
+    return user;
+  };
+
+  signIn = async () => {
+    const { username } = this.state;
+    const { navigation } = this.props;
+
+    try {
+      await this.checkUserExists(username);
+      await this.saveUser(username);
+      navigation.navigate('Repositories');
+    } catch (error) {
+      console.tron.log(error);
+    }
+  };
+
+  render() {
+    const { username } = this.state;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Bem-Vindo</Text>
+        <Text style={styles.text}>
+          Para continuar precisamos que informe o seu usuario do github
+        </Text>
+
+        <View style={styles.form}>
+          <TextInput
+            style={styles.input}
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Digite seu Usuario"
+            underlineColorAndroid="transparent"
+            value={username}
+            onChangeText={text => this.setState({ username: text })}
+          />
+        </View>
+
+        <TouchableOpacity style={styles.button} onPress={this.signIn}>
+          <Text style={styles.buttonText}>Prosseguir</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
